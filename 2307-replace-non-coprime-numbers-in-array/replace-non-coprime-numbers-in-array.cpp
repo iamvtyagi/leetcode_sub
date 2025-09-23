eb3c8d7a65c0b27d518d0fffbf31cffd36e4595e
vansh tyagi <23it178@jssaten.ac.in>
2025-09-23 19:48:05 +0530

    Time: 32 ms (17.19%) | Memory: 127.9 MB (22.73%) - LeetSync

--- 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.cpp
+++ 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
     vector<int> replaceNonCoprimes(vector<int>& nums) {
-        vector<int> stk;
-        for (int num : nums) {
-            while (!stk.empty() && gcd(stk.back(), num) > 1) {
-                num = lcm(stk.back(), num);
-                stk.pop_back();
+        stack<int> stk;
+        int n = nums.size();
+        stk.push(nums[0]);
+        for (int i = 1; i < n; i++) {
+            int curr = nums[i];
+            while (!stk.empty()) {
+                if (gcd(stk.top(), curr) > 1) {
+                    int val = stk.top();
+                    stk.pop();
+                    curr = lcm(val, curr);
+                } else {
+                    stk.push(curr);
+                    break;
+                }
             }
-            stk.push_back(num);
+            if (stk.empty()) stk.push(curr); // ensure curr is pushed
         }
-        return stk;
+        vector<int> ans;
+        while (!stk.empty()) {
+            ans.push_back(stk.top());
+            stk.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
