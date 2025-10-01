8268b64b2216b4a318e157721d3882365d29742f
vansh tyagi <23it178@jssaten.ac.in>
2025-10-01 18:34:05 +0530

    Time: 521 ms (5.25%) | Memory: 29.6 MB (5.44%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,54 +1,38 @@
 class Solution {
 public:
+   void printvec(vector<int> &veci){
+        for(int i = 0;i<veci.size();i++){
+              cout<<veci[i]<<" ";
+        }
+        cout<<endl;
+   }
     int trap(vector<int>& height) {
-        vector<int> prefixMax;
-        vector<int> suffixMax;
-        vector<int> mini;
-        int ans = 0;
         int n = height.size();
-        prefixMax.push_back(-1);
-        int leftMeiBada = height[0];
-        for(int i = 1;i<n;i++){
-            prefixMax.push_back(leftMeiBada);
-            leftMeiBada = max(leftMeiBada,height[i]);
+        vector<int> leftMax;
+        vector<int> rightMax;
+        int lefti = height[0];
+        int righti = height[n-1];
+        //  leftMax.push_back(lefti);
+        //  rightMax.push_back(righti);
+        for(int i = 0;i<n;i++){
+             lefti = max(lefti , height[i]);
+             leftMax.push_back(lefti);
+             righti = max(righti , height[n-i-1]);
+             rightMax.push_back(righti);
         }
-        suffixMax.push_back(-1);
-        int rightMeiBada = height[n-1];
-        for(int i = n-2;i>=0;i--){
-            suffixMax.push_back(rightMeiBada);
-            rightMeiBada = max(rightMeiBada,height[i]);
+        reverse(rightMax.begin(),rightMax.end());
+        printvec(leftMax);
+        printvec(rightMax);
+        vector<int> mini;
+        for(int i = 0;i<n;i++){
+            int val = min(leftMax[i],rightMax[i]);
+            mini.push_back(val);
         }
-        reverse(suffixMax.begin(),suffixMax.end());
-        // cout<<prefixMax.size()<<endl;
-        // cout<<suffixMax.size()<<endl;
-
-        for(int i = 0;i<prefixMax.size();i++){
-             int minii = min(prefixMax[i],suffixMax[i]);
-             mini.push_back(minii);
+        printvec(mini);
+        int ans = 0;
+        for(int i = 0;i<n;i++){
+            ans += mini[i] - height[i];
         }
-
-        //  for(int i = 0;i<prefixMax.size();i++){
-        //     cout<<prefixMax[i]<<" ";
-        //   }
-        //    cout<<endl;
-
-        
-        //  for(int i = 0;i<prefixMax.size();i++){
-        //     cout<<suffixMax[i]<<" ";
-        //   }
-        //    cout<<endl;
-        //  for(int i = 0;i<prefixMax.size();i++){
-        //     cout<<mini[i]<<" ";
-        //   }
-        //    cout<<endl;
-        // cout<<mini.size()<<endl;
-        
-       for(int i =0;i<height.size();i++){
-            if((mini[i]-height[i]) > 0) {
-                ans += (mini[i]-height[i]);
-            }
-       }
-
         return ans;
-      }
+    }
 };
