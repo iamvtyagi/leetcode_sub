a27775d97cb9f8d340970d1f115686603717e8fa
vansh tyagi <23it178@jssaten.ac.in>
2025-10-08 20:39:33 +0530

    Time: 7 ms (8.61%) | Memory: 63.1 MB (12.87%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -4,21 +4,16 @@ public:
         int n = height.size();
         int i = 0;
         int j = n-1;
-        long long ans = 0;  
-        
-        while(i < j){
-            int mini = min(height[i], height[j]);
-            long long width = j - i;  
-            long long area = 1LL * mini * width;
-            ans = max(ans, area);
-
-            // move the smaller height pointer
+        int area = INT_MIN;
+        while(i<=j){
+            int x = min(height[i],height[j]);
+            area = max(x*(j-i) , area);
             if(height[i] < height[j]){
                 i++;
-            } else {
+            }else{
                 j--;
             }
         }
-        return (int)ans; 
+        return area;
     }
 };
