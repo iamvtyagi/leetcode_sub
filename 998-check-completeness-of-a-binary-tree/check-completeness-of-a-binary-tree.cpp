348b96c1de7ac43f230b827f3c6ff5129928422a
vansh tyagi <23it178@jssaten.ac.in>
2025-10-10 10:56:56 +0530

    Time: 1 ms (6.80%) | Memory: 13.6 MB (78.10%) - LeetSync

--- 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.cpp
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
+   int countOfNodes(TreeNode* root){
+      if(root == NULL) return 0;
+      return 1 + countOfNodes(root->left) + countOfNodes(root->right);
+   }
+   
+    bool dfs(TreeNode* root , int i , int total){
+         if(root == NULL) return true;
+         if(i > total ) return false;
+         return dfs(root->left , 2*i , total) && dfs(root->right, 2*i + 1 , total);
+    }
     bool isCompleteTree(TreeNode* root) {
-        queue<TreeNode*> qt;
-        qt.push(root);
-        bool past = false;
-        while(!qt.empty()){
-            TreeNode* node = qt.front();
-            qt.pop();
-            if(node==NULL) past = true;
-            else {
-                if(past==true){
-                    return false;
-                }else{
-                    qt.push(node->left);
-                    qt.push(node->right);
-                }
-            }
-        }
-        return true;
+        int n = countOfNodes(root);
+        int i = 1;
+        return dfs(root,i,n); 
     }
 };
+
+// treated cbt as an array like in heap we treat array as heap 
