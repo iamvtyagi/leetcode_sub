bf12db0b079cf4ea3575d93c2e9aa4d062ba6a10
vansh tyagi <23it178@jssaten.ac.in>
2025-10-21 16:41:08 +0530

    Time: 27 ms (6.61%) | Memory: 59.9 MB (6.37%) - LeetSync

--- 415-add-strings/add-strings.cpp
+++ 415-add-strings/add-strings.cpp
@@ -1,44 +1,20 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        int n = num1.size();
-        int m = num2.size();
-        int i = n - 1;
-        int j = m - 1;
+        int i = num1.size() - 1;
+        int j = num2.size() - 1;
         int carry = 0;
         string res = "";
 
-        while(i >= 0 && j >= 0) {
-            int a = num1[i] - '0';
-            int b = num2[j] - '0';
+        while(i >= 0 || j >= 0 || carry > 0) {
+            int a = (i >= 0) ? num1[i] - '0' : 0;
+            int b = (j >= 0) ? num2[j] - '0' : 0;
             int sum = a + b + carry;
-            int digit = sum % 10;
             carry = sum / 10;
+            int digit = sum % 10;
             res = to_string(digit) + res;
             i--;
             j--;
-        }
-
-        while(i >= 0) {
-            int a = num1[i] - '0';
-            int sum = a + carry;
-            int digit = sum % 10;
-            carry = sum / 10;
-            res = to_string(digit) + res;
-            i--;
-        }
-
-        while(j >= 0) {
-            int b = num2[j] - '0';
-            int sum = b + carry;
-            int digit = sum % 10;
-            carry = sum / 10;
-            res = to_string(digit) + res;
-            j--;
-        }
-
-        if(carry > 0) {
-            res = to_string(carry) + res;
         }
 
         return res;
