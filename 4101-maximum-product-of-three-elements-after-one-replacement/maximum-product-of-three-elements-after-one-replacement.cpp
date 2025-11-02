a326de62509b7912279b6db8d619576c0c720c11
vansh tyagi <23it178@jssaten.ac.in>
2025-11-02 13:29:20 +0530

    Time: 49 ms (64.29%) | Memory: 124.2 MB (100.00%) - LeetSync

--- 4101-maximum-product-of-three-elements-after-one-replacement/maximum-product-of-three-elements-after-one-replacement.cpp
+++ 4101-maximum-product-of-three-elements-after-one-replacement/maximum-product-of-three-elements-after-one-replacement.cpp
@@ -1,39 +1,11 @@
 class Solution {
 public:
     long long maxProduct(vector<int>& nums) {
-    sort(nums.begin(), nums.end(), [](int a, int b) {
-        return abs(a) < abs(b);
-    });
-    unordered_map<int,int> mp;
-    for(auto& el : nums){
-        mp[abs(el)]++;
-    }
-        int n  = nums.size();
-        // find three sbse bade no
-        int phela = INT_MIN;
-        int doosra = INT_MIN;
-        int teesra = INT_MIN;
-        for(int i = 0;i<n;i++){
-            if(abs(nums[i]) > phela){
-                teesra = doosra;
-                doosra = phela;
-                phela = abs(nums[i]);
-            }
-        }
-        if(mp[phela]>=3){
-            teesra = phela;
-            doosra = phela ;
-        }else if(mp[phela]==2){
-            teesra = doosra;
-            doosra = phela;
-        }else if(mp[doosra]>=2){
-            teesra = doosra;
-        }
-        cout<<phela<<" "<<doosra<<" "<<teesra;
-   // step 2 : find smallest in these threee already done 
-   // strep 3: convert smallest to 10^5
-      teesra = 1e5;
-      long long ans = 1LL * phela * doosra * teesra;
-      return ans;
+        int n = nums.size();
+        sort(nums.begin(), nums.end(), [](int a, int b) {
+            return abs(a) < abs(b);
+        });
+        if(nums[n-3]==0 && nums[n-2]==0) return 0;
+        return 1LL*abs(nums[n-1]) * abs(nums[n-2])*1e5;        
     }
 };
