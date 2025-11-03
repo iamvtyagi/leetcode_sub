0982335628adaff8adba9be099499353a90abdd9
vansh tyagi <23it178@jssaten.ac.in>
2025-11-03 13:51:09 +0530

    Time: 4 ms (59.28%) | Memory: 99.7 MB (88.00%) - LeetSync

---
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minCost(string nums, vector<int>& neededTime) {
+        int n = nums.size();
+        if(n==1) return 0;
+        int cost = 0;
+        int i = 0;
+        int j = 1;
+        while(j<n && i<n){
+            if(nums[i]-'0' == nums[j]-'0'){
+                // bool flag = false;
+                if(neededTime[i]>neededTime[j]){
+                    // flag = true;
+                    cost += neededTime[j];
+                    neededTime[j]= -1;
+                    j++;
+                }else{
+                    cost += neededTime[i];
+                    j++;
+                    i = j-1;
+                }
+            }else{
+                i++;
+                while(neededTime[i]==-1){
+                    i++;
+                }
+                j++;
+                if(i==j) j++;
+            }
+        }
+        return cost;
+    }
+};
