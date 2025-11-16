c1140b037ac12432a038c12fd289757b1bb751ff
vansh tyagi <23it178@jssaten.ac.in>
2025-11-16 14:11:14 +0530

    Time: 1 ms (45.50%) | Memory: 11.7 MB (11.57%) - LeetSync

--- 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
     int numSub(string s) {
-        int  n = s.size();
+        long long cnt = 0, ans = 0;
         const long long MOD = 1e9+7;
-        vector<long long> nums;
-        int conti = 0;
-        for(int i = 0;i<n;i++){
-            if(s[i]=='1'&& i != n-1) {
-                conti++;
-            }else if(s[i]=='1'&& i == n-1){
-                conti++;
-                nums.push_back(conti);
-                cout<<conti<<endl;
-                conti = 0;
-            }
-            else{
-                if(conti != 0){
-                nums.push_back(conti);
-                cout<<conti<<endl;
-                conti = 0;
-                }
+
+        for(char c : s) {
+            if(c == '1') {
+                cnt++;                  
+            } else {
+                ans += (cnt * (cnt + 1) / 2) % MOD; 
+                cnt = 0;              
             }
         }
-        long long ans = 0;
-        for(int i = 0;i<nums.size();i++){
-            long long el = nums[i];
-           nums[i] =   ((el*(el+1))/2)%MOD;
-        }
-        for(int i = 0;i<nums.size();i++){
-            ans += nums[i];
-        }
-        return ans;
+
+        ans += (cnt * (cnt + 1) / 2) % MOD;
+
+        return ans % MOD;
     }
 };
