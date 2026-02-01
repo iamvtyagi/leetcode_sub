8f7aa72f372078ef9905e04c898d5784eb3d131d
vansh tyagi <23it178@jssaten.ac.in>
2026-02-02 01:40:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (8.33%) - LeetSync

--- 4194-count-monobit-integers/count-monobit-integers.cpp
+++ 4194-count-monobit-integers/count-monobit-integers.cpp
@@ -1,22 +1,23 @@
 class Solution {
-    bool checkNo(int n){
-        int totalBits = 0;
-        int temp = n;
-        while (temp > 0) {
-            totalBits++;
-            temp = temp >> 1;   
-        }   
-        int ones = __builtin_popcount(n);
-        int zeros = ones - totalBits;  
-        if(!ones || !zeros) return true;
-        return false;
-    }
+    // bool checkNo(int n){
+    //     int totalBits = 0;
+    //     int temp = n;
+    //     while (temp > 0) {
+    //         totalBits++;
+    //         temp = temp >> 1;   
+    //     }   
+    //     int ones = __builtin_popcount(n);
+    //     int zeros = ones - totalBits;  
+    //     if(!ones || !zeros) return true;
+    //     return false;
+    // }
 public:
     int countMonobit(int n) {
-        int cnt = 0;
-        for(int i = 0;i<=n;i++){
-            if(checkNo(i)) cnt++;
-        }
-        return cnt;
+        // int cnt = 0;
+        // for(int i = 0;i<=n;i++){
+        //     if(checkNo(i)) cnt++;
+        // }
+        // return cnt;
+        return 1 + log2(n+1);
     }
 };
