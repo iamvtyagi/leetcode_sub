9fba282075f31004ee0589225220c8bb504cc776
vansh tyagi <23it178@jssaten.ac.in>
2026-02-02 01:39:20 +0530

    Time: 3 ms (16.67%) | Memory: 8.5 MB (50.00%) - LeetSync

---
+++ 4194-count-monobit-integers/count-monobit-integers.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    bool checkNo(int n){
+        int totalBits = 0;
+        int temp = n;
+        while (temp > 0) {
+            totalBits++;
+            temp = temp >> 1;   
+        }   
+        int ones = __builtin_popcount(n);
+        int zeros = ones - totalBits;  
+        if(!ones || !zeros) return true;
+        return false;
+    }
+public:
+    int countMonobit(int n) {
+        int cnt = 0;
+        for(int i = 0;i<=n;i++){
+            if(checkNo(i)) cnt++;
+        }
+        return cnt;
+    }
+};
