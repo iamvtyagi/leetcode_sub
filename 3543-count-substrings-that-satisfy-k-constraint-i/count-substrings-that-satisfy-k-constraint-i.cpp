a3ccd40759b4bb0b0d360338524e73fc7f5b4655
vansh tyagi <23it178@jssaten.ac.in>
2026-02-17 23:21:52 +0530

    Time: 2 ms (30.57%) | Memory: 8.7 MB (91.34%) - LeetSync

---
+++ 3543-count-substrings-that-satisfy-k-constraint-i/count-substrings-that-satisfy-k-constraint-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countKConstraintSubstrings(string s, int k) {
+        int cnt = 0;
+        int n = s.size();
+        for(int i = 0;i<n;i++){
+            int cnt0 = 0;
+            int cnt1 = 0;
+            for(int j = i;j<n;j++){
+                if(s[j]=='0'){
+                    cnt0++;
+                }else{
+                    cnt1++;
+                }
+                if(cnt0 <= k || cnt1 <= k){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+};
