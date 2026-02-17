ab3d89a85f651237f78af1d237c6dc83e7804d99
vansh tyagi <23it178@jssaten.ac.in>
2026-02-18 00:13:08 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (41.32%) - LeetSync

---
+++ 401-binary-watch/binary-watch.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> readBinaryWatch(int turnedOn) {
+        vector<string> res;
+
+        for(int h = 0;h<=11;h++){
+            for(int m = 0;m<=59;m++){
+                if(__builtin_popcount(h) + __builtin_popcount(m) == turnedOn){
+                    string hrs = to_string(h);
+                  string mins =  (m<10?"0":"")+to_string(m);
+                  res.push_back(hrs+":"+mins);
+                }
+            }
+        }
+        return res;
+    }
+};
