32773aae84673b8341991254828f01036223145e
vansh tyagi <23it178@jssaten.ac.in>
2026-02-23 22:40:52 +0530

    Time: 16 ms (59.70%) | Memory: 25.1 MB (61.02%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
-string generate(string& word){
-    int arr[26] = {0};
-    for(auto& ch : word){
-         arr[ch-'a']++;
-    }
-
-    string new_word = "";
-
-    for(int i = 0;i<26;i++){
-         int freq = arr[i];
-
-         if(freq > 0){
-             new_word += string(freq,i+'a');
-         }
-    }
-    return new_word;
-}
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         int n = strs.size();
+        unordered_map<string ,vector<string>> mp;
         vector<vector<string>> ans;
-        unordered_map<string , vector<string>> mp;
+
         for(int i = 0;i<n;i++){
-             string temp = strs[i];
-
-             string new_string = generate(temp);
-
-             mp[new_string].push_back(temp);
+            string s = strs[i];
+            sort(s.begin(),s.end());
+            mp[s].push_back(strs[i]);
         }
 
-        for(auto it : mp){
-            ans.push_back(it.second);
+        for(auto& el : mp){
+            ans.push_back(el.second);
         }
         return ans;
     }
