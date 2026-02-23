33db5bbc6323ec65666f2a9789dfe6ed57868443
vansh tyagi <23it178@jssaten.ac.in>
2026-02-23 22:16:22 +0530

    Time: 7 ms (19.64%) | Memory: 9.9 MB (10.49%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,21 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(t.begin(),t.end());
-        sort(s.begin(),s.end());
-        return (s==t);
+        int n = s.size();
+        int m = t.size();
+        if(n != m) return false;
+        unordered_map<int,int> mp;
+        for(int i = 0;i<n;i++){
+            mp[s[i]]++;
+        }
+        for(int i = 0;i<n;i++){
+            if(mp.find(t[i]) != mp.end()){
+                mp[t[i]]--;
+                if(mp[t[i]] == 0) mp.erase(t[i]);
+            }else{
+                return false;
+            }
+        }
+        return mp.empty();
     }
 };
