c58a91f1ce3f3a44fd0cc0659846833210f83376
vansh tyagi <23it178@jssaten.ac.in>
2026-02-24 20:38:18 +0530

    Time: 8 ms (72.43%) | Memory: 18.6 MB (39.27%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -2,28 +2,29 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int n = s.size();
-        string ans = "";
-        for(int i = 0;i<n;i++){
-            for(int j = i;j<n;j++){
-                int k = i;
-                int l = j;
-                bool check = true;
-                while(k<l){
-                    if(s[k]==s[l]){
-                        k++;
-                        l--;
-                    }else{
-                        check = false;
-                        break;
-                    }
-                }
-                if(check){
-                if(ans.size() < (j-i+1)){
-                   ans = s.substr(i, j - i + 1);
-                }
-                }
+        string res = "";
+
+        for(int i=0;i<n;i++){
+            // Odd length palindrome
+            int st=i,end=i;
+            while(st>=0&&end<n&&s[st]==s[end]){
+                st--;
+                end++;
             }
+            string temp=s.substr(st+1,end-st-1);
+            if(temp.size()>res.size())res=temp;
+
+            // Even length palindrome
+            st=i;
+            end=i+1;
+            while(st>=0&&end<n&&s[st]==s[end]){
+                st--;
+                end++;
+            }
+            temp=s.substr(st+1,end-st-1);
+            if(temp.size()>res.size())res=temp;
         }
-        return ans;
+
+        return res;
     }
 };
