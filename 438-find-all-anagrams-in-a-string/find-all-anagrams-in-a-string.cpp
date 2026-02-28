63bf353016f64f1d18193582953769727d9dead6
vansh tyagi <23it178@jssaten.ac.in>
2026-02-28 12:49:06 +0530

    Time: 30 ms (14.46%) | Memory: 16.3 MB (11.04%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
-    vector<int> findAnagrams(string s, string p) {
-        int n = s.size(), m = p.size();
-        if (n < m) return {};
-
+    vector<int> findAnagrams(string s2, string s1) {
+        int n = s1.size();
+        int m = s2.size();
         vector<int> ans;
-        vector<int> freqP(26, 0), freqS(26, 0);
-
-
-        for (char c : p) freqP[c - 'a']++;
-
-        for (int i = 0; i < m; i++) {
-            freqS[s[i] - 'a']++;
+        if(m<n){
+            return ans;
         }
-        if (freqS == freqP) ans.push_back(0);
-
-        for (int i = m; i < n; i++) {
-            freqS[s[i] - 'a']++;               
-            freqS[s[i - m] - 'a']--;           
-
-            if (freqS == freqP) ans.push_back(i - m + 1);
+        unordered_map<char , int> mp;
+        for(int i = 0;i<n;i++){
+            mp[s1[i]]++;
         }
-        return ans;
+        int cnt = mp.size();
+        int i = 0;
+        int j = 0;
+        while(j<m){
+            mp[s2[j]]--;
+            if(mp[s2[j]]==0){
+                mp.erase(s2[j]);
+                cnt--;
+            }
+            if(j-i+1<n){
+                j++;
+            }else if(j-i+1==n){
+                if(mp.size()==0 && cnt==0){
+                    ans.push_back(i);
+                }
+                    mp[s2[i]]++;
+                    if(mp[s2[i]]==1) cnt++;
+                    if(mp[s2[i]]==0) mp.erase(s2[i]);
+                    i++;
+                    j++;
+            }
+        }
+        return ans; 
     }
 };
