3cb6120a33720d721f58ce913655773084246575
vansh tyagi <23it178@jssaten.ac.in>
2026-03-03 08:37:14 +0530

    Time: 30 ms (37.78%) | Memory: 59.9 MB (28.71%) - LeetSync

---
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string getsn(int n){
+        if(n==1){
+            return "0";
+        }else if(n==2){
+            return "011";
+        }
+      string st = getsn(n-1);
+      string s = st;
+     for(char &c : s) c = (c == '0') ? '1' : '0';
+      reverse(s.begin(),s.end());
+      return st+"1"+s;
+    }
+    char findKthBit(int n, int k) {
+        unordered_map<int,string> mp;
+        mp[1]= "0";
+        mp[2]= "011";
+        string st = getsn(n);
+        cout<<st<<endl;
+        return st[k-1];
+    }
+};
