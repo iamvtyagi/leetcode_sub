2a28944e2edfea79d7cd89e78f0374e9a65708ba
vansh tyagi <23it178@jssaten.ac.in>
2026-04-20 15:41:31 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (34.57%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,55 +1,37 @@
 class Solution {
-    int pivot(vector<int> &arr) {
-        int n = arr.size();
-        int s = 0, e = n - 1, mid;
-        while (s < e) {
-            mid = s + (e - s) / 2;
-            if ( mid<e && arr[mid] > arr[mid + 1]) {
-                return mid; 
-            } 
-            else if( mid>s && arr[mid] < arr[mid - 1] ){
-                return mid-1;
-            }
-            else if(arr[s]>= arr[mid]){
-                e = mid-1;
-            }
-            else if(arr[s]<arr[mid]){
-                s = mid + 1;
-            }
-        }
-        return -1;
-    }
 public:
-    int search(vector<int>& arr, int target) {
-        int peak = pivot(arr);
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int s = 0;
+        int e = n-1;
+        while(s <= e){
+            int mid = (s) + (e-s)/2;
 
-      
-        int s = 0, e = peak, mid;
-        while (s <= e) {
-            mid = s + (e - s) / 2;
-            if (arr[mid] > target) {
-                e = mid - 1;
-            } else if (arr[mid] < target) {
-                s = mid + 1;
-            } else {
+            if(nums[mid]==target){
                 return mid;
             }
-        }
 
-      
-        s = peak + 1;
-        e = arr.size() - 1;
-        while (s <= e) {
-            mid = s + (e - s) / 2;
-            if (arr[mid] > target) {
-                e = mid - 1;
-            } else if (arr[mid] < target) {
-                s = mid + 1;
-            } else {
-                return mid;
+            //check left part sorted or not
+           if(nums[s] <= nums[mid]) {
+                //check target  exist in this array or not
+                if(nums[s] <= target && target < nums[mid]){
+                     e = mid-1;
+                }else{
+                    s = mid+1;
+                }
+
+
+            }
+
+         // check right part is soted or not -> atleast one part is alwasy sorted in one time rotated array 
+            else{
+                    if(nums[mid]<target && nums[e]>=target){
+                        s = mid+1;
+                    }else{
+                        e = mid-1;
+                    }
             }
         }
-
         return -1;
     }
 };
