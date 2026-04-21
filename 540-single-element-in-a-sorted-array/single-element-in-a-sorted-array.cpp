795dc65c391d59c6ec036c2255b4924e94df1b24
vansh tyagi <23it178@jssaten.ac.in>
2026-04-21 13:48:41 +0530

    Time: 0 ms (100.00%) | Memory: 26 MB (99.83%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -4,31 +4,37 @@ public:
         int n = nums.size();
         int l = 0;
         int r = n-1;
-        while(l<r){
-            int mid = l + (r-l)/2;
 
-          // ye batayega ki right side mei even no of el h ya odd
-            bool isEven;
-            if((r-mid)%2==0){
-                isEven = true;
-            }else{
-                isEven = false;
+        if(n==1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[n-2] != nums[n-1]) return nums[n-1];
+
+        while(l<=r){
+            int mid = (l) + (r-l)/2;
+
+            //check got ans or not
+            if(mid>0 && mid<n-1 && nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
+                return nums[mid];
             }
-          //yha esne mid ne just right vale se pairing krli 
-            if(nums[mid]==nums[mid+1]){
-                if(isEven){
-                    l = mid+1;
+
+            //cal len of left and right
+            int len = n-mid-1;
+            //check odd or even and act accordingly
+            if(len%2==0){
+                //here check matching kidhar se hori 
+                if(mid>0 && nums[mid]==nums[mid-1]){
+                    r = mid-1;
                 }else{
-                    r = mid -1;
+                    l = mid+1;
                 }
             }else{
-                if(isEven){
-                    r = mid;
-                }else{
+                if(mid>0 && nums[mid]==nums[mid-1]){
                     l = mid+1;
+                }else{
+                    r = mid-1;
                 }
             }
         }
-        return nums[r];
+        return 0;
     }
 };
