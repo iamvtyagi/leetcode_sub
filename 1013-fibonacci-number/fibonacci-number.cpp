1a5b3d47467c44f453b5b197853f71b0d869ddd5
vansh tyagi <23it178@jssaten.ac.in>
2026-04-23 11:11:11 +0530

    Time: 12 ms (32.14%) | Memory: 7.7 MB (64.98%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,17 +1,7 @@
 class Solution {
 public:
-    int t[31];
-    int helper(int n ){
-         if(n<=1){
-            return n;
-         }
-         if(t[n] != -1){
-            return t[n];
-         }
-         return t[n] =  helper(n-1) + helper(n-2);
-    }
     int fib(int n) {
-        memset(t,-1,sizeof(t));
-        return helper(n);
+        if(n<=1) return n;
+        return fib(n-1) + fib(n-2);
     }
 };
