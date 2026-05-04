ecb964a22cfdb0433dbec9893ed205eae3806c87
vansh tyagi <23it178@jssaten.ac.in>
2026-05-04 19:01:07 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (58.91%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -4,24 +4,27 @@ public:
         int n = nums1.size();
         int m = nums2.size();
 
-        for(int i = 0; i < n; i++) {
-            int el = nums1[i];
-            bool found = false;
-            bool greaterFound = false;
+        stack<int> s;
+        unordered_map<int,int> mp;
 
-            for(int j = 0; j < m; j++) {
-                if(nums2[j] == el) {
-                    found = true;
-                }
-
-                if(found && nums2[j] > el) {
-                    nums1[i] = nums2[j];  
-                    greaterFound = true;
-                    break;
-                }
+        //fill v with next greater to right
+        // start from the right loop mei kyuki yrr sbse phele right greate mei left el check hona hai 
+        for(int i = m - 1; i >= 0; i--) {
+            while(!s.empty() && s.top() <= nums2[i]) {
+                s.pop();
             }
 
-            if(!greaterFound) nums1[i] = -1;  
+            if(s.empty()) {
+                mp[nums2[i]] = -1;
+            } else {
+                mp[nums2[i]] = s.top();
+            }
+
+            s.push(nums2[i]);
+        }
+
+        for(int i = 0;i<n;i++){
+            nums1[i] = mp[nums1[i]];
         }
         return nums1;
     }
